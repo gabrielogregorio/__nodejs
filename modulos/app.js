46cdacd4fa39814d4d4bde26fd8f568dcfe0ec40
Gabriel Gregório <gabriel.gregorio.1@outlook.com>
2021-10-12 09:18:19 -0300

    módulos

---
+++ modulos/app.js
@@ -0,0 +1,5 @@
+var calculadora = require("./calculadora.js");
+
+console.log(calculadora.somar(10,20));
+calculadora.PI = calculadora.PI + 10
+console.log(calculadora.PI-10);
---
+++ modulos/calculadora.js
@@ -0,0 +1,11 @@
+function somar(a, b) {
+  return a + b;
+}
+
+const PI = 3.1415;
+
+// Disponibilizar para acesso externo
+module.exports = {
+  somar,
+  PI
+};
